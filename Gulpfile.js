b836b79a123edcf76c6c522ceaf165b248f851de
jrusl <jrusl@outlook.com>
2016-03-28 19:00:05 +0300

    Added some ui

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,28 +2,35 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var u = require('gulp-uglify');
 var jade = require('gulp-jade');
+var clean = require('gulp-clean');
 
-gulp.task('ts', function() {
-  return gulp.src('./src/ts/*.ts')
-		.pipe(ts({
-			noImplicitAny: true,
-			out: 'lp.js'
-		}))
-    .pipe(u())
-    .pipe(gulp.dest('./dist/js/'));
+gulp.task('clean', function() {
+  return gulp.src('./dist', {read: false})
+    .pipe(clean());
 });
 
-gulp.task('jade', function() {
-  return gulp.src('./src/jade/*.jade')
+// Typescript
+gulp.task('ts', ['clean'], function() {
+  return gulp.src('./src/ts/**/*.ts')
+		.pipe(ts({
+	     out: 'lp.js'
+     }))
+     .pipe(u()) // Uglify JS
+    .pipe(gulp.dest('./dist/js/')); // Copy it
+});
+
+// Jade
+gulp.task('jade', ['clean'], function() {
+  return gulp.src('./src/jade/**/*.jade') // Compile it
     .pipe(jade({
       locals: {}
     }))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./dist/')); // Copy it
 });
 
-gulp.task('css', function() {
-  return gulp.src('./src/css/*.css')
-    .pipe(gulp.dest('./dist/css'));
+gulp.task('static', ['clean'], function() {
+  return gulp.src('./src/static/**/*.*')
+    .pipe(gulp.dest('./dist/')); // Copy static
 });
 
-gulp.task('default', ['ts', 'jade', 'css']);
+gulp.task('default', ['clean', 'ts', 'jade', 'static']);

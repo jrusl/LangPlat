9e734cc1b7d64d7feb207b91b9a6707a83aa6b4f
jrusl <jrusl@outlook.com>
2016-03-28 14:43:52 +0300

    Configured gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,29 @@
 var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var u = require('gulp-uglify');
+var jade = require('gulp-jade');
 
-gulp.task('default', function() {
-  // Testing gulp
-  Will it pass? I don't know
+gulp.task('ts', function() {
+  return gulp.src('./src/ts/*.ts')
+		.pipe(ts({
+			noImplicitAny: true,
+			out: 'lp.js'
+		}))
+    .pipe(u())
+    .pipe(gulp.dest('./dist/js/'));
 });
+
+gulp.task('jade', function() {
+  return gulp.src('./src/jade/*.jade')
+    .pipe(jade({
+      locals: {}
+    }))
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('css', function() {
+  return gulp.src('./src/css/*.css')
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('default', ['ts', 'jade', 'css']);
